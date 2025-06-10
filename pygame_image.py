4192b9ac58b07119aa69e3d0f2c455b7ffc69e04
c0a2412885 <c0a2412885@edu.teu.ac.jp>
2025-06-10 15:41:52 +0900

    背景伸ばし

--- pygame_image.py
+++ pygame_image.py
@@ -18,7 +18,9 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [tmr*-1, 0])
+        screen.blit(bg_img, [tmr*-1+1600, 0])
         screen.blit(kk_img, [300, 200])
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)
