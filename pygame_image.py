d83e7e7f761b9a1f2ea7fee748034ff62d1d7072
c0a2412885 <c0a2412885@edu.teu.ac.jp>
2025-06-10 16:05:55 +0900

    rectのcenter属性に初期座標を設定

--- pygame_image.py
+++ pygame_image.py
@@ -25,7 +25,7 @@ def main():
         screen.blit(bg_img, [tmr*-1, 0])
         screen.blit(bg_img2, [tmr*-1+1600, 0])
         screen.blit(bg_img, [tmr*-1+3200, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img,kk_rct)
 
         pg.display.update()
         tmr += 1      
